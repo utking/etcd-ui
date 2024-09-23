f763247058829b0d47530bfe0c5326f651b07ca4
Gennady Utkin <utkingg@gmail.com>
2024-09-23 18:36:48 -0500

    Showing if the cluster has auth enabled

--- internal/providers/etcd/v3/client.go
+++ internal/providers/etcd/v3/client.go
@@ -1,12 +1,16 @@
 package v3
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/utking/etcd-ui/internal/helpers/utils"
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -119,4 +123,62 @@ func New(
 
 func (c *Client) Close() {
 	c.client.Close()
+}
+
+// AuthEnabled will use a random username/password and analyze the reposne
+// of reading a random key. Seems to be the best way to check if auth is en/dis-abled.
+// We still need to know it we use TLS or not though.
+func CheckAuthEnabled(
+	endpoints []string,
+	sslCertFile, sslKeyFile, sslCAFile string,
+) (bool, error) {
+	var (
+		username = fmt.Sprintf("tmp-%x", time.Now().Unix())
+		password = fmt.Sprintf("pwd-%x", time.Now().Unix())
+		key      = fmt.Sprintf("/fake/test/auth/key-%x", time.Now().Unix())
+	)
+
+	tmpClient, clntErr := New(endpoints, sslCertFile, sslKeyFile, sslCAFile, username, password)
+	if clntErr != nil {
+		switch {
+		case strings.Contains(clntErr.Error(), "authentication failed, invalid user ID or password"):
+			return true, nil
+		case strings.Contains(clntErr.Error(), "authentication is not enabled"):
+			return false, nil
+		default:
+			// We will have to implement it when it's found
+			log.Printf("error %q is not implemented. Please report it\n", clntErr.Error())
+
+			return false, clntErr
+		}
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), utils.GetOpTimeout())
+	defer cancel()
+
+	getKeyResp, err := tmpClient.client.Get(ctx, key)
+
+	if err == nil {
+		// The chance to guess name/pass/key is too slim to get no error unless there is no auth
+		// The response should contain no keys
+		if getKeyResp != nil && len(getKeyResp.Kvs) == 0 {
+			return false, nil
+		}
+
+		// Either the response is nil of there are keys in the response. Edge case to investigate later
+		return false, fmt.Errorf("this should not have happened, retry please")
+	}
+
+	// We should not have gone here, so let's check the error again and see. Investigate when happend
+	switch {
+	case strings.Contains(err.Error(), "authentication failed, invalid user ID or password"):
+		return true, nil
+	case strings.Contains(err.Error(), "authentication is not enabled"):
+		return false, nil
+	default:
+		// We will have to implement it when it's found
+		log.Printf("error %q is not implemented. Please report it\n", err.Error())
+
+		return false, err
+	}
 }
--- internal/providers/etcd/v3/roles.go
+++ internal/providers/etcd/v3/roles.go
@@ -125,6 +125,7 @@ func (c *Client) AddRole(
 	}
 
 	fmt.Println("Done for", name)
+
 	return errors.Join(errList...)
 }
 
